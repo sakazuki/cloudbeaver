ff8894cba2871b9799e7c477662acc45d3d2ee4e
sakazuki <sakazuki@gmail.com>
2022-04-06 20:24:31 +0000

    wip

--- deploy/build.sh
+++ deploy/build.sh
@@ -21,9 +21,7 @@ mkdir ./cloudbeaver/web
 
 echo "Clone dbeaver platform"
 
-cd .
 [ ! -d dbeaver ] && git clone https://github.com/dbeaver/dbeaver.git
-cd cloudbeaver/deploy
 
 echo "Build CloudBeaver server"
 
